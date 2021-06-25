9c08cbe66c4f35c01abe2dcdbf0ac30a4709fee7
ericgeagan <82694625+eric-geagan@users.noreply.github.com>
2021-06-25 02:16:17 -0700

    fixed serverside data

--- jQuery-App/public/javascripts/script.js
+++ jQuery-App/public/javascripts/script.js
@@ -1,4 +1,5 @@
 let list = []
+let id = 0
 class Show {
   constructor(pname, ptype, pstatus, prating, pURL) {
     this.name = pname
@@ -6,118 +7,201 @@ class Show {
     this.status = pstatus
     this.rating = prating
     this.URL = pURL
-  }
-
-  getAll() {
-    return this.type + ': "' + this.name + '", ' + 'Current Status: '+ this.status +  ', Rating: ' + this.rating
+    this.id = id++
   }
 }
 
+function makeString(item) {
+  return item.type + ': "' + item.name + '", ' + 'Current Status: '+ item.status +  ', Rating: ' + item.rating
+}
+
 $(document).on("pagebeforeshow","#list",function() {
-  showEntries()
+  showServerEntries()
 });
 
 $(document).on('pagebeforeshow','#stats',function() {
   showValues()
 })
 
-function formSubmitEvent() {
-  // Get name of movie and rating
+// function formSubmitEvent() {
+//   // Get name of movie and rating
+//   let showName = $('#showName').val();
+//   let type = $('#type').val();
+//   let status = $('#status').val();
+//   let rating = $('#rating').val();
+//   let url = $('#url').val();
+//   let show = new Show(showName, type, status, parseInt(rating), url)
+//   // Validate the object values?
+//   let result = true
+//   if (result) {
+//     // Add object to list
+//     list.push(show)
+    
+//     // Set fields to blank string
+//   $('#showName').val('');
+//   $('#url').val('');
+//   } else {
+//     // Alert of bad values
+//     alert("Please fill in the fields with a valid entry.")
+//   }
+// }
+
+function addServerItem() {
   let showName = $('#showName').val();
   let type = $('#type').val();
   let status = $('#status').val();
   let rating = $('#rating').val();
   let url = $('#url').val();
   let show = new Show(showName, type, status, parseInt(rating), url)
-  // Validate the object values?
-  let result = true
-  if (result) {
-    // Add object to list
-    list.push(show)
-    
-    // Set fields to blank string
+
+  $.ajax({
+    url: '/addItem',
+    type: 'POST',
+    data: JSON.stringify(show),
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    success: function(result) {
+      console.log(result)
+    }
+  })
   $('#showName').val('');
   $('#url').val('');
-  } else {
-    // Alert of bad values
-    alert("Please fill in the fields with a valid entry.")
-  }
 }
 
-function showEntries() {
-  let parent = $('#listId')
-  parent.empty() // Removes all existing child elements
-  // Adds all list elements to display
-  list.forEach(item => {
-    let text = item.getAll()
-    // Check if a url was entered
-    if (item.URL === '') {
-      // Default to imdb search
-      const searchItem = item.name.replace(/\s/g, '+')
-      item.URL = 'https://www.imdb.com/find?q=' + searchItem + '&ref_=nv_sr_sm'
+// function showEntries() {
+//   let parent = $('#listId')
+//   parent.empty() // Removes all existing child elements
+//   // Adds all list elements to display
+//   list.forEach(item => {
+//     let text = item.getAll()
+//     // Check if a url was entered
+//     if (item.URL === '') {
+//       // Default to imdb search
+//       const searchItem = item.name.replace(/\s/g, '+')
+//       item.URL = 'https://www.imdb.com/find?q=' + searchItem + '&ref_=nv_sr_sm'
+//     }
+//     let newElement = document.createElement('li')
+//     let listItem = document.createElement('li')
+//     let deleteButton = document.createElement('button')
+//     let content = document.createElement('div')
+
+//     content.addEventListener('click', 
+//       function (event) {
+//         event.preventDefault()
+//         if (confirm('You are about to open a new window. Please confirm.')) {
+//           window.open(item.URL)
+//         }
+//       },
+//       false)
+//     content.innerHTML = text
+//     content.className = 'itemText'
+//     deleteButton.onclick = function() { 
+//       let listIndex = Array.prototype.indexOf.call(newElement.parentNode, newElement)
+//       list.splice(0, 1)
+//       newElement.parentNode.removeChild(newElement) 
+//     }
+//     deleteButton.innerHTML = 'X'
+
+//     newElement.appendChild(deleteButton)
+//     newElement.appendChild(content)
+//     newElement.className = 'item'
+
+//     // Add it to the unordered list
+//     parent.append(newElement);
+//   })
+// }
+
+function showValues() {
+  $.get('/getList', function(data, status) {
+    list = data
+    if (list.length) {
+      let all = $('.stat').map(function() {
+        return this
+      }).get()
+      all.forEach(item => {
+        item.innerHTML = ''
+      })
+      // Calculate average show score
+      let totalScore = list.reduce(((accumulator, currentValue) => accumulator + currentValue.rating), 0)
+      $('#averageScore').append(Math.round(totalScore / list.length * 10) / 10)
+      // Sum number of movies watched
+      let numMovies = list.reduce(((accumulator, currentValue) => currentValue.type == 'Movie' && currentValue.status !== 'Plan to Watch' ? accumulator + 1 : accumulator), 0)
+      $('#numMovies').append(numMovies)
+      // Sum number of TV Series watched
+      let numTV = list.reduce(((accumulator, currentValue) => currentValue.type !== 'Movie' && currentValue.status !== 'Plan to Watch' ? accumulator + 1 : accumulator), 0)
+      $('#numTV').append(numTV)
+      // List number of shows Finished
+      let numFinished = list.reduce(((accumulator, currentValue) => currentValue.status == 'Completed' ? accumulator + 1 : accumulator), 0)
+      $('#numFinished').append(numFinished)
+      // List number of shows currently watching
+      let numWatching = list.reduce(((accumulator, currentValue) => currentValue.status == 'Watching' ? accumulator + 1 : accumulator), 0)
+      $('#numWatching').append(numWatching)
+      // List number of shows Planning to watch
+      let numPlanToWatch = list.reduce(((accumulator, currentValue) => currentValue.status == 'Plan to Watch' ? accumulator + 1 : accumulator), 0)
+      $('#numPlanToWatch').append(numPlanToWatch)
     }
-    let newElement = document.createElement('li')
-    let listItem = document.createElement('li')
-    let deleteButton = document.createElement('button')
-    let content = document.createElement('div')
-
-    content.addEventListener('click', 
-      function (event) {
-        event.preventDefault()
-        if (confirm('You are about to open a new window. Please confirm.')) {
-          window.open(item.URL)
-        }
-      },
-      false)
-    content.innerHTML = text
-    content.className = 'itemText'
-    deleteButton.onclick = function() { 
-      let listIndex = Array.prototype.indexOf.call(newElement.parentNode, newElement)
-      list.splice(0, 1)
-      newElement.parentNode.removeChild(newElement) 
-    }
-    deleteButton.innerHTML = 'X'
-
-    newElement.appendChild(deleteButton)
-    newElement.appendChild(content)
-    newElement.className = 'item'
-
-    // Add it to the unordered list
-    parent.append(newElement);
   })
 }
 
-function showValues() {
-  if (list.length) {
-    let all = $('.stat').map(function() {
-      return this
-    }).get()
-    all.forEach(item => {
-      item.innerHTML = ''
+function showServerEntries() {
+  let parent = $('#listId')
+  parent.empty() // Removes all existing child elements
+
+  $.get('/getList', function(data, status) {
+    list = data
+    console.log(data)
+    list.forEach(item => {
+      let text = makeString(item)
+      // Check if a url was entered
+      if (item.URL === '') {
+        // Default to imdb search
+        const searchItem = item.name.replace(/\s/g, '+')
+        item.URL = 'https://www.imdb.com/find?q=' + searchItem + '&ref_=nv_sr_sm'
+      }
+      let newElement = document.createElement('li')
+      let listItem = document.createElement('li')
+      let deleteButton = document.createElement('button')
+      let content = document.createElement('div')
+  
+      content.addEventListener('click', 
+        function (event) {
+          event.preventDefault()
+          if (confirm('You are about to open a new window. Please confirm.')) {
+            window.open(item.URL)
+          }
+        },
+        false)
+      content.innerHTML = text
+      content.className = 'itemText'
+      deleteButton.onclick = function() {
+        let listIndex = item.id
+        // list.splice(0, 1)
+        // console.log(listIndex)
+        $.ajax({
+          type: 'DELETE',
+          url: '/deleteItem/' + listIndex,
+          success: function(result) {
+            // console.log(result)
+            // document.location.href = 'index.html#list'
+            newElement.parentNode.removeChild(newElement) 
+          },
+          error: function(xhr, textStatus, errorThrown) {
+            console.log('Error')
+            alert('Server could not delete movie with id: '+ listIndex)
+          }
+        })
+      }
+      deleteButton.innerHTML = 'X'
+  
+      newElement.appendChild(deleteButton)
+      newElement.appendChild(content)
+      newElement.className = 'item'
+  
+      // Add it to the unordered list
+      parent.append(newElement);
     })
-    // Calculate average show score
-    let totalScore = list.reduce(((accumulator, currentValue) => accumulator + currentValue.rating), 0)
-    $('#averageScore').append(Math.round(totalScore / list.length * 10) / 10)
-    // Sum number of movies watched
-    let numMovies = list.reduce(((accumulator, currentValue) => currentValue.type == 'Movie' && currentValue.status !== 'Plan to Watch' ? accumulator + 1 : accumulator), 0)
-    $('#numMovies').append(numMovies)
-    // Sum number of TV Series watched
-    let numTV = list.reduce(((accumulator, currentValue) => currentValue.type !== 'Movie' && currentValue.status !== 'Plan to Watch' ? accumulator + 1 : accumulator), 0)
-    $('#numTV').append(numTV)
-    // List number of shows Finished
-    let numFinished = list.reduce(((accumulator, currentValue) => currentValue.status == 'Completed' ? accumulator + 1 : accumulator), 0)
-    $('#numFinished').append(numFinished)
-    // List number of shows currently watching
-    let numWatching = list.reduce(((accumulator, currentValue) => currentValue.status == 'Watching' ? accumulator + 1 : accumulator), 0)
-    $('#numWatching').append(numWatching)
-    // List number of shows Planning to watch
-    let numPlanToWatch = list.reduce(((accumulator, currentValue) => currentValue.status == 'Plan to Watch' ? accumulator + 1 : accumulator), 0)
-    $('#numPlanToWatch').append(numPlanToWatch)
-  }
+  })
 }
-
-
-
 
 
 
--- jQuery-App/routes/index.js
+++ jQuery-App/routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+serverList = []
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/getList', function(req, res) {
+  res.status(200).json(serverList)
+})
+
+router.post('/addItem', function(req, res) {
+  const newItem = req.body
+  serverList.push(newItem)
+  res.status(200)
+})
+
+router.delete('/deleteItem/:id', (req, res) => {
+  const id = req.params.id
+  // console.log('deleteing ID: ' + id)
+  let found = false
+  for(let i = 0; i < serverList.length; i++) {
+    // console.log(serverList[i].id, id)
+    if (serverList[i].id == id) {
+      serverList.splice(i, 1)
+      found = true
+      break;
+    }
+  }
+  if (!found) {
+    // console.log('not found')
+    return res.status(500).json({
+      status: 'error'
+    })
+  } else {
+    res.status(200).json('deleted item')
+  }
+})
 
 module.exports = router;
